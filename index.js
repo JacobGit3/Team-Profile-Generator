0949f714e66d997785269e3bd9333cd47d3dba01
Jacob <jacob.tobin.liberty@gmail.com>
2022-01-27 08:12:24 -0500

    created questions file, created all constructors, created index.js functionality and generator functionality is working

---
+++ index.js
@@ -0,0 +1,72 @@
+// Importing constructors, questions, and inquire
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js')
+const Intern = require ('./lib/Intern.js')
+const inquirer = require('inquirer');
+const Questions = require ('./dist/Questions.js');
+
+//Global Variables
+let team = []
+
+// Creates a Manager, Ran on load
+function CreateManager() {
+  // Prompts with questions for profile data
+  inquirer.prompt(Questions.manager)
+    .then(function newEmployee(data){
+      // Creates manager using Manager Constructor
+      let newManager = new Manager(data.name, data.id, data.email, data.office);
+      team.push(newManager);
+      CreateTeam();
+    })
+}
+
+// After Manager is created Team options are displayed
+function CreateTeam() {
+  // Prompts with options to add members or complete file
+  inquirer.prompt(Questions.team)
+    .then(function choice(data){
+      // If chosen to add member, run function for associated role desired
+      if (data.addMember){
+        if (data.role === 'Engineer'){
+          CreateEngineer()
+        }else if (data.role === 'Intern'){
+          CreateIntern()
+        }else {
+          console.log('Error')
+        }
+      }
+      // If chosen no to add member the team is complete, create HTML
+      else {
+        CreatePage();
+      }
+    })
+}
+
+// Creates an Engineer Profile
+function CreateEngineer() {
+  inquirer.prompt(Questions.engineer)
+    .then(function makeEngineer(data){
+      let newEngineer = new Engineer(data.name, data.id, data.email, data.github);
+      team.push(newEngineer);
+      console.log(team);
+      CreateTeam();
+    })
+}
+
+// Creates an Intern Profile
+function CreateIntern() {
+  inquirer.prompt(Questions.intern)
+  .then(function makeIntern(data){
+    let newIntern = new Intern(data.name, data.id, data.email, data.school);
+    team.push(newIntern);
+    CreateTeam();
+  })
+}
+
+// Creates the Page HTML and copies over the CSS
+function CreatePage() {
+  console.log('Page Created');
+}
+
+// Ran on load to start team generator
+CreateManager()
